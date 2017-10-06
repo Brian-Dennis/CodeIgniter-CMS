1b8fb391187a20a7751ac2af1e7e7b066aa36b40
Brian Dennis <briandennis49@outlook.com>
2017-10-05 17:55:34 -0700

    added database data
    added data variable to the template.

--- application/controllers/admin/Dashboard.php
+++ application/controllers/admin/Dashboard.php
@@ -3,7 +3,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dashboard extends CI_Controller {
 	public function index(){
+		// Get Activities
+		$data['activities'] = $this->Activity_model->get_list();
+
 		// Load template
-		$this->template->load('admin', 'default', 'dashboard');
+		$this->template->load('admin', 'default', 'dashboard', $data);
 	}
 }
